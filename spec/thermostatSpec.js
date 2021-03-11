26bfbfa1db4dc57e6859bda996249261c6828848
Hannah Desmond <desmond.hannah@googlemail.com>
2021-03-11 14:59:31 +0000

    Add test to check there is a default temperature

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Thermostat', function() {
+  var thermostat
+  beforeEach(function() {
+    thermostat = new Thermostat;
+  })
+  it('has a default of 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature).toEqual(20);
+  })
+})
